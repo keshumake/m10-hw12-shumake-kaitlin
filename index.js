7132e0ddb57ddb91bf94183b24515b3ac9fe3f49
Kaitlin Shumake <shumakek@ufl.edu>
2022-04-19 21:13:49 -0400

    created class and methods

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // YOUR CODE HERE
+
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        console.log("BEEP BEEP!")
+    }
+    performMaintenance() {
+        setTimeout()
+    }
+
+    let mySweetRide = new Car("Pontiac", "Fiero", "1988")
+    
+
+}
